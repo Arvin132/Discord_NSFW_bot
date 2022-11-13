9981c0d925d158a28639542206eae31ce8c00b57
Arvin132 <asgharianarvin@gmail.com>
2022-11-13 14:37:06 -0500

    implemented !@set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");  
 const config = require("./config.json")
 const fs = require("fs")
-const setChannels = require("./setChannels.json")
+let setChannels = require("./setChannels.json")
 
 const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds,
                                              Discord.GatewayIntentBits.GuildMessages,
@@ -10,17 +10,16 @@ const client = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds,
 client.on('messageCreate', async function(message) {
     if (message.author.bot || !message.content.startsWith(config.PREFIX)) {
         // if the message if from another bot or if doesn't have the prefix
-        console.log("nope")
         return;
     }
 
 
     let realMessage = message.content.substring(config.PREFIX.length);
     let tokens = realMessage.split(" ");
-    let command = tokens.pop();
+    let command = tokens.at(0);
+    tokens.splice(0,1)
     let channel = client.channels.cache.get(message.channelId)
     // below are the commands that can be given to the bot
-
     if (command == "info") {
         channel.send("Hello My Name is Harambe, and i am a bot created to detect NSFW content")
         channel.send("I am in the process of development")
@@ -28,7 +27,6 @@ client.on('messageCreate', async function(message) {
     }
 
     if (command == "set") {
-        console.log("hello")
         let allChannels = [client.channels.cache.at(0)];
         let CHANNELS = [""]
         CHANNELS.pop()
@@ -36,14 +34,20 @@ client.on('messageCreate', async function(message) {
         client.channels.cache.each(canal => allChannels.push(canal))
 
         tokens.forEach(token => {
+            let found = false
             allChannels.forEach(chan => {
                 if (chan.name == token) {
                     CHANNELS.push(chan.id)
+                    found = true
                 }
             })
+
+            if (!found) {
+                message.reply(token +" is not a valid channel")
+            }
         })
 
-        console.log(CHANNELS)
+        setChannels = CHANNELS
         
         fs.writeFile("./setChannels.json", JSON.stringify(CHANNELS), err => {
             if (err) {
@@ -52,6 +56,19 @@ client.on('messageCreate', async function(message) {
                 console.log('Successfully wrote file')
             }
         })
+
+        message.reply("The channels below are set for detection:")
+        let data  = ""
+        CHANNELS.forEach(str => {
+            client.channels.cache.forEach(chn => {
+                if (chn.id == str) {
+                    data += chn.name + ", "
+                    
+                }
+            })
+        });
+        data = data.slice(0, -2)
+        channel.send(data)
     }
 
     // later 
@@ -62,7 +79,7 @@ client.on('messageCreate', async function(message) {
 })
 
 client.on("messageCreate", function(message) {
-    if (message.attachments.size > 0 && setChannels.CHANNELS.includes(message.channelId)) { // the message has an attachment
+    if (message.attachments.size > 0 && setChannels.includes(message.channelId)) { // the message has an attachment
         message.attachments.each(attachment => {
             console.log(attachment.url)
         })
